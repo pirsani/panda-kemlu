7ae22981f8e4ccb0de7b2af0595e4b7dca16fec9
devnext <devnext@pirsani.id>
2024-09-14 22:59:50 +0200

    update login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,27 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-full">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 sm:p-24 w-full">
+      <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
         <div className="flex flex-col w-full">
           <h1 className="text-4xl font-bold">Panda</h1>
           <p className="text-lg font-light">
-            Aplikasi Pengelolan Administrasi Perbendaharaan
+            Pengelolan Administrasi Perbendaharaan
           </p>
-          <div className="py-10 flex flex-row gap-6 h-auto">
-            <div className="w-1/2 h-96 relative">
+          <div className="py-10 flex flex-col sm:flex-row gap-6 h-auto">
+            <div className="w-full sm:w-1/2 h-52 sm:h-96 relative">
               <Image
                 src="/images/hero-1.png"
                 alt="Panda"
-                layout="fill"
-                objectFit="cover"
-                className="rounded-lg"
+                fill
+                className="rounded-lg object-cover"
                 priority
                 placeholder="blur"
                 blurDataURL="/images/hero-1-blur.png" // Add a small, low-quality version of the image
               />
             </div>
 
-            <div className="w-1/2 h-auto flex flex-grow">
+            <div className="w-full sm:w-1/2 h-auto flex flex-grow">
               <Suspense>
                 <LoginForm />
               </Suspense>
